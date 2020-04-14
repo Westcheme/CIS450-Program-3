ab11f599242ca4a1d68623b440bd12f9fa6b8942
JuliusCesarEstrope <juliuscesarme@gmail.com>
2020-04-14 19:18:26 -0400

    Comment required functionality of DiskAPI

--- Project3/Project3/DiskAPI.cpp
+++ Project3/Project3/DiskAPI.cpp
@@ -1,26 +1,52 @@
 #include "DiskAPI.h"
 
+//This method is called exactly once by the OS before any other disk operations take place
 int DiskAPI::Disk_Init()
 {
 
+	//Initialize all data in sector to 0
+
+
+	//Create superblock and empty root directory entries
+
+
+	return 0;
+
 }
 
+//This method is called to load the contents of the external disk file system array
+//into the working disk. This should be executed once while booting (in FS_Boot())
 int DiskAPI::Disk_Load()
 {
 
+
+	return 0;
 }
 
+//This method saves the current in-memory working disk to the external disk.
+//It saves the contents of an in-memory file system to the external file disk
+//to be "booted" again later. This method should be invoked by FS_Sync
 int DiskAPI::Disk_Save()
 {
+	
 
+	return 0;
 }
 
+//Write the data in the buffer to the specified sector.
 int DiskAPI::Disk_Write(int sector, string buffer)
 {
 
+
+	//If the indicated sector is out of bounds, or the buffer is null,
+	//set the diskErrMsg attribute in Simulation to "E_READ_INVALID_PARAM"
 }
 
+//This method reads a specified sector into a specified buffer. The buffer and sector are assumed to be the same size.
 int DiskAPI::Disk_Read(int sector, string buffer)
 {
 
+
+	//If the indicated sector is out of bounds, or the buffer is null,
+	//set the diskErrMsg attribute in Simulation to "E_READ_INVALID_PARAM"
 }
